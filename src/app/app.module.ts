2b64188918302d83fb9538d25e9ee64f46d557db
Jared <jmattgiroux@gmail.com>
2023-10-25 15:10:55 -0500

    App module | Added authModule to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule.forRoot({
+      domain: 'development-playground.us.auth0.com',
+      clientId: 'EBviEdZguoZOL7j8K6e9u1kxYoK05bR3',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
