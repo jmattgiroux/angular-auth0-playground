dcf67656d047c8e8e77e82b49b3e62a9c9197f0d
Jared <jmattgiroux@gmail.com>
2023-10-26 12:18:16 -0500

    App Module | Added HttpClientModule to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AuthModule } from '@auth0/auth0-angular';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent],
@@ -12,12 +13,13 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AuthModule.forRoot({
       domain: 'development-playground.us.auth0.com',
-      clientId: 'EBviEdZguoZOL7j8K6e9u1kxYoK05bR3',
+      clientId: 'yZ0gEhejrVPJSxjYCpdn99Or4fj38Oe0',
       authorizationParams: {
         redirect_uri: window.location.origin,
       },
     }),
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
