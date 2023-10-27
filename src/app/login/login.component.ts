a1b9d434313f61117a277bff86f782a99eea6860
Jared <jmattgiroux@gmail.com>
2023-10-27 11:24:10 -0500

    Login Component | Added addTestRoleToUser, not fully working yet

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import axios from 'axios';
 
 @Component({
   selector: 'app-login',
@@ -60,5 +61,37 @@ export class LoginComponent implements OnInit {
         console.error(error);
       },
     );
+  }
+
+  addTestRoleToUser() {
+    console.log('Trying to add test role to other user');
+    const roleId = 'rol_n3aWDhpbtVoCvPXf';
+    const url = `https://login.auth0.com/api/v2/roles/${roleId}/users`;
+    const userId = 'auth0|653bda2e9b73f78151a79469';
+    const users = [userId];
+
+    let data = JSON.stringify({
+      users: users,
+    });
+
+    let config = {
+      method: 'POST',
+      maxBodyLength: Infinity,
+      url: url,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.token}`,
+      },
+      data: data,
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
