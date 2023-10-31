e3a483077b2daffef4b0886348c5335d08c8d592
Antonio Saucedo <antoniojesus@byui.edu>
2023-10-31 14:38:34 -0600

    Module | Added ConfigModule

--- m2m-app/src/app.module.ts
+++ m2m-app/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule} from "@nestjs/config";
 import { ManagementClient } from "auth0";
 
 @Module({
-  imports: [ManagementClient],
+  imports: [ConfigModule.forRoot(), ManagementClient],
   controllers: [AppController],
   providers: [AppService],
 })
