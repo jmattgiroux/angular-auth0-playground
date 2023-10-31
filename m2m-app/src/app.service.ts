cd61d4b4496ba94e522cf1eb529f338366e3b31f
Jared <jmattgiroux@gmail.com>
2023-10-31 16:13:27 -0500

    App Service | Simplified constructor

--- m2m-app/src/app.service.ts
+++ m2m-app/src/app.service.ts
@@ -4,20 +4,10 @@ import {ManagementClient} from "auth0";
 
 @Injectable()
 export class AppService {
-  managementClient: ManagementClient;
 
   getHello(): string {
     return 'Hello World!';
   }
 
-  constructor() {
-    const domain = process.env.DOMAIN;
-    console.log(`domain: ${domain}`)
-    const ManagementClient = require('auth0').ManagementClient;
-    this.managementClient = new ManagementClient({
-      domain: process.env.DOMAIN,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET
-    })
-  }
+  constructor(private managementClient: ManagementClient) {}
 }
