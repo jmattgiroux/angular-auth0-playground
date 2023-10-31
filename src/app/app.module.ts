152b57600869b2f7fc10e6cc712733acea259c33
Jared <jmattgiroux@gmail.com>
2023-10-31 12:43:22 -0500

    Module | Added callback to redirect uri

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
       domain: 'development-playground.us.auth0.com',
       clientId: 'EBviEdZguoZOL7j8K6e9u1kxYoK05bR3',
       authorizationParams: {
-        redirect_uri: window.location.origin,
+        redirect_uri: window.location.origin + '/callback',
         audience: 'https://people-service-epsilon.com',
         scope: 'openid profile offline_access',
       },
@@ -27,3 +27,8 @@ import { HttpClientModule } from '@angular/common/http';
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+/***
+
+ http://localhost:4200/callback?error=invalid_request&error_description=client%20requires%20organization%20membership%2C%20but%20user%20does%20not%20belong%20to%20any%20organization&state=Mk5FOF8zVXBlUjBCYlBmaTJPckR4QVZsQ3hiQUpENDlkS3huZ1lUTHEuVw%3D%3D
+ */
