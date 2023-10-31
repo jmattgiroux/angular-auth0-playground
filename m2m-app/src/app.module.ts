9cbe8784400e117700bf696a0a9664954b339fc1
Antonio Saucedo <antoniojesus@byui.edu>
2023-10-31 14:05:55 -0600

    Module | Added ManagementClient to imports

--- m2m-app/src/app.module.ts
+++ m2m-app/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ManagementClient } from "auth0";
 
 @Module({
-  imports: [],
+  imports: [ManagementClient],
   controllers: [AppController],
   providers: [AppService],
 })
