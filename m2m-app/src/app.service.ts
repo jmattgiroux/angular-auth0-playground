f86ad3b86b791e19f792978fa6fb9c77b3d77ed4
Jared <jmattgiroux@gmail.com>
2023-10-31 14:50:21 -0500

    App Service | Injected Management Client into AppService

--- m2m-app/src/app.service.ts
+++ m2m-app/src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ManagementClient } from 'auth0';
+
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  constructor(private  managementClient: ManagementClient) {
+    this.managementClient = new ManagementClient({
+      domain: process.env.DOMAIN,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET
+    });
   }
 }
