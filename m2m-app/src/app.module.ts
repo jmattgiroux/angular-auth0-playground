fe2ae9fb574c2df308b957106307699d5a4f31d4
Jared <jmattgiroux@gmail.com>
2023-10-31 16:12:01 -0500

    Module | Added ManagementClient

--- m2m-app/src/app.module.ts
+++ m2m-app/src/app.module.ts
@@ -1,11 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
 import {ConfigModule} from "@nestjs/config";
+import {ManagementClient} from "auth0";
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: ManagementClient,
+    useFactory: () => {
+      return new ManagementClient({
+        domain: process.env.DOMAIN,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+      });
+    }
+  }],
 })
-export class AppModule {}
+export class AppModule {
+}
