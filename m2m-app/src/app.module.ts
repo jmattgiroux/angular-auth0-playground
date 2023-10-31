f0eca7710d4570f46ab2737660340b95ef2210fa
Jared <jmattgiroux@gmail.com>
2023-10-31 15:49:45 -0500

    Module | Removed Management Client temporarily

--- m2m-app/src/app.module.ts
+++ m2m-app/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule} from "@nestjs/config";
-import { ManagementClient } from "auth0";
 
 @Module({
-  imports: [ConfigModule.forRoot(), ManagementClient],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
